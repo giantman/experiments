3637ffd39d05e2734476fd13013614c1ad3aad58
giantman <giantmanco@gmail.com>
2026-06-25 15:39:56 -0700

    nav and type

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,17 @@
 import { Link, NavLink } from 'react-router-dom'
+import starIcon from '../assets/star.svg'
 
 export default function Nav() {
   return (
     <nav className="grid grid-cols-[auto_1fr_1fr_auto] gap-8 px-4 pt-4 pb-6 items-start">
-      <Link to="/" className="text-sm text-[#1a1917] hover:opacity-50 transition-opacity leading-snug">
-        Giant Man
+      <Link
+        to="/"
+        className="inline-flex items-center gap-1.5 text-sm text-[#1a1917] hover:opacity-50 transition-opacity leading-snug"
+      >
+        <img src={starIcon} alt="" width={12} height={12} className="shrink-0" />
       </Link>
 
-      <div className="flex flex-col">
+      <div className="flex gap-4">
         <NavLink
           to="/work"
           className={({ isActive }) =>
@@ -28,7 +32,6 @@ export default function Nav() {
 
       <div className="text-sm text-[#1a1917]/50 leading-snug">
         Design &amp; Engineering<br />
-        Visual Experiments
       </div>
 
       <a
